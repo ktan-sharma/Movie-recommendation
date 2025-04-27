0768a5e5d7bb43518b8dcfc4685d6c3e4cd3e48d
ktan-sharma <ketan950200@gmail.com>
2025-04-27 18:30:09 +0530

    Debug: print route map and confirm file loaded on deploy

--- recommend_api.py
+++ recommend_api.py
@@ -21,5 +21,8 @@ def get_recommendations():
 def root():
     return 'API is running!'
 
+print("recommend_api.py loaded!")
+print(app.url_map)
+
 if __name__ == '__main__':
     app.run(debug=True)
